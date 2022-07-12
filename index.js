ee4e224a77b24964166c2ff0aeb64d9c918a65a5
EasyPage <74344815+easypage@users.noreply.github.com>
2022-07-12 15:09:05 +0900

    test 코드 추가

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/", async function (req, res) {
-  console.log(req.body);
   if (Object.keys(req.body).length === 0) {
     return res.status(400).send({ status: "실패하였습니다." });
   }
+  console.log("body확인");
   console.log(req.body);
 
   const data = await axios.get(req.body.addr).then((result) => {
-    console.log(result);
-    return res.status(200).send(result);
+    return res.status(200).send({ data: result.data });
   });
+});
+
+app.get("/test", async function (req, res) {
+  const data = await axios.get().then((result) => {});
 });
